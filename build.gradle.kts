d9b60b2c42484fa8ddff59758ac65ff519178681
Mike Halma <mike.halma@digital.justice.gov.uk>
2020-05-05 10:48:07 +0100

    DT-807 Use latest versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "0.1.1"
-  kotlin("plugin.spring") version "1.3.71"
-  kotlin("plugin.jpa") version "1.3.71"
+  kotlin("plugin.spring") version "1.3.72"
+  kotlin("plugin.jpa") version "1.3.72"
 }
 
 configurations {
@@ -18,7 +18,7 @@ dependencies {
   implementation("io.springfox:springfox-bean-validators:2.9.2")
 
   runtimeOnly("com.h2database:h2:1.4.200")
-  runtimeOnly("org.flywaydb:flyway-core:6.3.2")
+  runtimeOnly("org.flywaydb:flyway-core:6.4.1")
   runtimeOnly("org.postgresql:postgresql:42.2.12")
 
   testImplementation("org.springframework.boot:spring-boot-starter-webflux")
