ec27f2eb8c9f6b1d0bdccfe091e82e77cc9ae61b
Peter Phillips <peter@greenthistle.co.uk>
2020-05-11 11:55:13 +0100

    DT-825: Upgrade spring boot

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
+import org.jetbrains.kotlin.backend.wasm.lower.excludeDeclarationsFromCodegen
+
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "0.1.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "0.3.0"
   kotlin("plugin.spring") version "1.3.72"
   kotlin("plugin.jpa") version "1.3.72"
 }
@@ -10,6 +12,7 @@ dependencyCheck {
 
 configurations {
   implementation { exclude(mapOf("module" to "tomcat-jdbc")) }
+  testImplementation { exclude(group = "org.junit.vintage") }
 }
 
 dependencies {
