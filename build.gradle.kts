caef7ab4ef36f4e60ea9589ecacdb5d1aed0ded3
petergphillips <peter@greenthistle.co.uk>
2020-09-03 12:05:32 +0100

    DT-1080: 🔒 Suppress kotlin milestone vuln (#37)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "1.0.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "1.0.2"
   kotlin("plugin.spring") version "1.4.0"
   kotlin("plugin.jpa") version "1.4.0"
-}
-
-dependencyCheck {
-  suppressionFiles = listOf() // Turn off dependency suppression files - none are currently relevant
 }
 
 configurations {
@@ -18,9 +14,9 @@ dependencies {
 
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.4.4")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.4.4")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.4.4")
+  implementation("org.springdoc:springdoc-openapi-ui:1.4.6")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.4.6")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.4.6")
 
   runtimeOnly("com.h2database:h2:1.4.200")
   runtimeOnly("org.flywaydb:flyway-core:6.5.5")
