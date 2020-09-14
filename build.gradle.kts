30253c102789a2b13dd089293d14f5524e065489
petergphillips <peter@greenthistle.co.uk>
2020-09-14 10:59:07 +0100

    DT-1109: ⬆️ Try out new gradle plugin (#40)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "1.0.2"
-  kotlin("plugin.spring") version "1.4.0"
-  kotlin("plugin.jpa") version "1.4.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "1.0.4"
+  kotlin("plugin.spring") version "1.4.10"
+  kotlin("plugin.jpa") version "1.4.10"
 }
 
 configurations {
