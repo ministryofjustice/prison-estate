83ebda69af8c084fe15acadeb3fc1161f93f2ff3
Peter Phillips <peter@greenthistle.co.uk>
2021-02-04 13:16:11 +0000

    DT-1442: ⬆️ Application insights v3.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.0.0-BETA-2"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.0.1"
   kotlin("plugin.spring") version "1.4.21"
   kotlin("plugin.jpa") version "1.4.21"
 }
@@ -14,12 +14,12 @@ dependencies {
 
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.5.2")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.2")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.2")
+  implementation("org.springdoc:springdoc-openapi-ui:1.5.3")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.3")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.3")
 
   runtimeOnly("com.h2database:h2:1.4.200")
-  runtimeOnly("org.flywaydb:flyway-core:7.4.0")
+  runtimeOnly("org.flywaydb:flyway-core:7.5.2")
   runtimeOnly("org.postgresql:postgresql:42.2.18")
 
   testImplementation("org.springframework.boot:spring-boot-starter-webflux")
